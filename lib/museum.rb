4288678f59d0556aac26556dfcc3d195718d455e
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-31 18:58:40 -0600

    Initialize Museum

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,25 +1,10 @@
 require 'pry'
 
 class Museum
-  attr_reader :name
-  attr_accessor :exhibits
+
+attr_reader :name
 
   def initialize(name)
-    @name = name
-    @exhibits = {}
+    @name = name 
   end
-
-  def add_exhibit(exhibit)
-    exhibits.map do |title, cost|
-    @exhibits[title.to_s] = cost.to_i
-  end
-
 end
-
-
-dmns = Museum.new("Denver Museum of Nature and Science")
-
-p dmns.name
-dmns.add_exhibit("Dead Sea Scrolls", 10)
-dmns.add_exhibit("Gems and Minerals", 0)
-p dmns.exhibits
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,24 +6,12 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
 
   def test_it_exist
-    m = Museum.new(name)
-    assert_instance_of Museum, m
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_instance_of Museum, dmns
   end
 
-  def test_it_can_have_a_name
-    m = Museum.new("Denver Museum of Nature and Science")
-    assert_equal "Denver Museum of Nature and Science", m.name
+  def test_it_has_a_name
+  dmns = Museum.new("Denver Museum of Nature and Science")
+  assert_equal "Denver Museum of Nature and Science", dmns.name
   end
-
-  def test_it_can_add_sing_exhibit
-    m = Museum.new("Denver Museum of Nature and Science")
-    assert_equal ["Dead Sea Scrolls" => 10],m.add_exhibit("Dead Sea Scrolls" => 10)
-  end
-
-  def test_it_can_add_additional_exhibit
-    m = Museum.new("Denver Museum of Nature and Science")
-    m.add_exhibit("Dead Sea Scrolls" => 10)
-    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], m.add_exhibit("Gems and Minerals" => 0)
-  end
-
 end
